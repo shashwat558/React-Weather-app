d08d220b0f61a756c383c02c7cecfc300e35ee8c
shashwat558 <tumbaktuhere@gmail.com>
2024-03-14 20:29:08 +0530

    Wrote the fetch api and UI part

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weather, setWeather] = useState({});
+  const [location, setLocation] = useState("Jamnagar");
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    ifClicked();
+  }, []);
+  function ifClicked() {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID={184eab55dec728f70d700dcc142423e1}&units=metric`
+    )
+      .then((res) => {
+        if(res.ok){
+          console.log(res.status);
+          return res.json();
+        } else {
+          if(res.status === 4000) {
+            return alert("Oops, there seems to be an error!(wrong location)")
+          }
+          alert("Oops, there seems to be an error!");
+          throw new Error("You have an error");
+        }
+        
+      })
+      .then((object) => {
+        setWeather(object);
+        console.log(weather);
+      })
+      .catch((error) => console.log(error));
+
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${location}&client_id={'k9NTLjDTigkJgSKZPX6-d6XS_PP7DrK1049wf_9-myU'}`
+    )
+      .then((res) => {
+        if(res.ok){
+          return res.json();
+        } else {
+          throw new Error("You madea a mistake")
+        }
+      }
+      )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section>
+      <div className='app'>
+        <div className='wrapper'>
+          <div className='search'>
+            <input type="text"
+            value={location} 
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder='Enter Location'
+            className='Location_input'
+            />
+            <button className='location_searcher' onClick={ifClicked}>
+              Search Location
+            </button>
+
+          </div>
+          <div className='app_data'>
+            <p className='temp'>Current Temprature: {weather?.main?.temp}</p>
+          </div>
+          <img className='app__image' src={photos} alt="" />
+        </div>
+        </div>           
+    </section>
   )
 }
 
---
+++ src/apiKey.js
@@ -0,0 +1,4 @@
+module.exports = {
+    key: "184eab55dec728f70d700dcc142423e1",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
