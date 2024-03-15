b50031023b233aa43dd457eafe4913521311c972
shashwat558 <tumbaktuhere@gmail.com>
2024-03-15 23:33:15 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,40 @@
 import './App.css'
 import React, { useEffect, useState } from 'react'
 
+
 function App() {
   const [weather, setWeather] = useState({});
   const [location, setLocation] = useState("Jamnagar");
-  const [photos, setPhotos] = useState([]);
+
   useEffect(() => {
-    ifClicked();
-  }, []);
+    ifClicked()
+
+  },[])
+
   function ifClicked() {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID={184eab55dec728f70d700dcc142423e1}&units=metric`
+        `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${'184eab55dec728f70d700dcc142423e1'}&units=metric`
     )
       .then((res) => {
         if(res.ok){
           console.log(res.status);
           return res.json();
         } else {
-          if(res.status === 4000) {
-            return alert("Oops, there seems to be an error!(wrong location)")
+          if(res.status === 400){
+            return alert("OOpsm there seems to be a problem(Wrong location)")
           }
-          alert("Oops, there seems to be an error!");
+          alert("Oops ther seems to be an error");
           throw new Error("You have an error");
         }
-        
-      })
-      .then((object) => {
-        setWeather(object);
-        console.log(weather);
-      })
-      .catch((error) => console.log(error));
 
-    fetch(
-      `https://api.unsplash.com/search/photos?query=${location}&client_id={'k9NTLjDTigkJgSKZPX6-d6XS_PP7DrK1049wf_9-myU'}`
-    )
-      .then((res) => {
-        if(res.ok){
-          return res.json();
-        } else {
-          throw new Error("You madea a mistake")
-        }
-      }
-      )
+      })
+        .then((Object) => {
+          setWeather(Object)
+          console.log(weather);
+        })
+         .catch((error) => console.log(error));
   }
+
 
   return (
     <section>
@@ -63,8 +55,9 @@ function App() {
           </div>
           <div className='app_data'>
             <p className='temp'>Current Temprature: {weather?.main?.temp}</p>
+            <p> {weather?.weather[0]?.main}</p>
           </div>
-          <img className='app__image' src={photos} alt="" />
+        
         </div>
         </div>           
     </section>
--- src/apiKey.js
+++ src/apiKey.js
@@ -1,4 +1,3 @@
 module.exports = {
     key: "184eab55dec728f70d700dcc142423e1",
-    base: "https://api.openweathermap.org/data/2.5/",
   };
